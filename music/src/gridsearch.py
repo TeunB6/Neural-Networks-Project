5048b08914beb1e73f6f510817e34f3e0ce2383f
TeunB06 <t.boersma.4@student.rug.nl>
2024-05-23 16:49:34 +0200

    gridsearch done

--- music/src/gridsearch.py
+++ music/src/gridsearch.py
@@ -10,13 +10,23 @@ class GridSearch:
         self.param_combinations = [zip(param_grid.keys(), v) for v in product(*param_grid.keys())]
         self.folds = folds
     
-    def __call__(self, X: np.ndarray, y: np.ndarray) -> Module:
+    def __call__(self, X: np.ndarray, y: np.ndarray) -> tuple[Module, int]:
+        results = []
         for param in self.param_combinations:
-            kFold=KFold(n_splits=10,random_state=42,shuffle=False)
+            kFold=KFold(n_splits=self.folds,shuffle=True)
             trial_model = self.model_class(**param)
             scores = []
             for train_index,test_index in kFold.split(X):                
                 X_train, X_test, y_train, y_test = X[train_index], X[test_index], y[train_index], y[test_index]
                 trial_model.fit(X_train, y_train)
                 scores.append(trial_model.score(X_test, y_test))
+            
+            mean_score = np.mean(scores)
+            results.append(trial_model, mean_score)
+        
+        final_model, final_score = max(results, key=lambda x: x[1])
+        return final_model, final_model
+        
+
+
                             
