7eef181341d5fc5e67356bc31fa6e50cdacf4fd8
TeunB06 <t.boersma.4@student.rug.nl>
2024-05-23 16:32:28 +0200

    restructure

---
+++ music/src/music_model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class RNNModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=1):
+        super(RNNModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.h2o = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        out, _ = self.rnn(x, h0)
+        out = self.h2o(out[:, -1, :])  # Take the last output
+        return out
+
+    def save_model(self, path):
+        torch.save(self.state_dict(), path)
+
+    def load_model(self, path):
+        self.load_state_dict(torch.load(path))
